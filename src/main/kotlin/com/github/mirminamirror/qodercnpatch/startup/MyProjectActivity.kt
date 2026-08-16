43e09637e449b9b2e29b2157914f67e533cb6037
GitHub Action <action@github.com>
2026-08-16 10:44:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/mirminamirror/qodercnpatch/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/mirminamirror/qodercnpatch/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.mirminamirror.qodercnpatch.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
